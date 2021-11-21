8e7c3faff9f1cc4bd1d71b310e4e62ee4f499576
foralost <kombii4@gmail.com>
2021-11-21 18:54:06 +0100

    Skip List: Adding removal and fixing inseting (using history)

--- btree/src/include/btree.h
+++ btree/src/include/btree.h
@@ -14,9 +14,10 @@
 #define TREE_NODE_POINTERS 	4
 #define TREE_NODE_KEYS		TREE_NODE_POINTERS-1
 #define TREE_INVALID_KEY	-1
+
 struct tree_node {
 	uint32_t			keys[TREE_NODE_KEYS] = { -1 };
-	struct 	tree_node* 	nodes[TREE_NODE_POINTERS] = {};
+	struct 	tree_node* 	ptrs[TREE_NODE_POINTERS] = {};
 	struct  tree_node*  root = NULL;
 };
 
@@ -24,17 +25,55 @@ struct tree_node {
 void btree_set_empty_node(struct tree_node* start)
 {
 	memset(start->keys, TREE_INVALID_KEY, TREE_NODE_KEYS * sizeof(*start->keys));
-	memset(start->nodes, 0, sizeof(start->nodes[0]) * TREE_NODE_POINTERS);
+	memset(start->ptrs, 0, sizeof(start->ptrs[0]) * TREE_NODE_POINTERS);
 }
 
 
-void btree_append_key(struct tree_node* node, const uint32_t val){
+char btree_is_node_full(struct tree_node* node){
+	for(uint8_t i = 0 ; i < TREE_NODE_KEYS; i++)
+		if(node->keys[i] == TREE_INVALID_KEY)
+			return 0;
+
+	return 1;
+}
+struct tree_node* btree_move_down(struct tree_node* node, const uint32_t val)
+{
+	for(uint8_t i = 0; i < TREE_NODE_KEYS; i++)
+		if( node->keys[i] < val && node->ptrs[i] )
+			return node->ptrs[i];
+		else if(node->keys[i] > val && node->ptrs[i+1])
+			return node->ptrs[i+1];
+
+	return 0;
+
+}
+struct tree_node* btree_append_key(struct tree_node* node, const uint32_t val){
+
+	struct tree_node* next_node = node, *curr_node = node;
+
+	// Find a leaf node
+	while(!next_node)
+	{
+		curr_node = next_node;
+		next_node = btree_move_down(curr_node, val);
+	}
 
 	for(uint8_t i = 0 ; i < TREE_NODE_KEYS - 1; i++){
 		if(val <= node->keys[i + 1] && node->keys[i] == TREE_INVALID_KEY ){
 			node->keys[i] = val;
+			return node;
 		}
 	}
+
+	struct tree_node* new_node = malloc(sizeof(struct tree_node));
+	btree_set_empty_node(new_node);
+	uint8_t break_index = TREE_NODE_KEYS << 1;
+
+	if( TREE_NODE_KEYS & 1 ){
+		break_index++;
+	}
+
+	new_node->keys[0] = val;
 
 	// There is no space left... time for splitting
 
--- skip_list/main.c
+++ skip_list/main.c
@@ -32,6 +32,11 @@ struct skip_list {
 	struct tower *top_tower;
 };
 
+struct insert_history {
+	struct item *inserted;
+	struct insert_history *next;
+};
+
 uint8_t head_or_tail() {
 	return rand() % 2;
 }
@@ -46,9 +51,9 @@ struct item* create_node(int val, struct item *bottom, struct tower *assigned) {
 	return toRet;
 }
 
-struct item* append_node(struct item *to_append, int val, struct item *bottom) {
+struct item* append_node(struct item *to_append, int val) {
 	struct item *temp = to_append->next;
-	to_append->next = create_node(val, bottom, to_append->assigned);
+	to_append->next = create_node(val, NULL, to_append->assigned);
 
 	if (temp)
 		to_append->next->next = temp;
@@ -76,23 +81,41 @@ struct skip_list* create_skip_list() {
 	return toRet;
 }
 
-//TODO: History of insertion in parameter
-//	to remove searching inside one tower
-struct item* search_to_insert(struct item *node, int val) {
+struct item* search_to_insert(struct item *node, int val,
+		struct insert_history **hist) {
 
 	struct item *curr = node; // Start at top-left node
 
 	for (;;) {
-		while (curr->next && val >= curr->next->val){
-			printf("Going next: %d -> %d, LVL: %d \n", curr->val, curr->next->val, curr->assigned->height);
+		while (curr->next && val >= curr->next->val) {
+			printf("Going next: %d -> %d, LVL: %d \n", curr->val,
+					curr->next->val, curr->assigned->height);
 			curr = curr->next;
 		}
 
-		if (curr->bottom){
-			printf("Going down: LVL %d -> %d\n", curr->assigned->height, curr->bottom->assigned->height);
+		if (curr->bottom) {
+			printf("Going down: LVL %d -> %d\n", curr->assigned->height,
+					curr->bottom->assigned->height);
+
+			//FIXME: Fix goddamn names
+			struct insert_history *new_item = malloc(
+					sizeof(struct insert_history));
+			new_item->inserted = curr;
+			new_item->next = NULL;
+
+			if (hist) {
+				if (!(*hist))
+					*hist = new_item;
+				else {
+					struct insert_history *temp = (*hist);
+					*hist = new_item;
+					new_item->next = temp;
+				}
+			}
+
 			curr = curr->bottom;
-		}
-		else{
+		} else {
+
 			return curr;
 		}
 	}
@@ -101,14 +124,14 @@ struct item* search_to_insert(struct item *node, int val) {
 	return NULL;
 }
 
-struct item* search_key(struct skip_list* list, int val){
+struct item* search_key(struct skip_list *list, int val) {
 	struct item *curr = list->top_tower->items; // Start at top-left node
 
 	for (;;) {
 		while (curr->next && val >= curr->next->val)
 			curr = curr->next;
 
-		if(curr->val == val)
+		if (curr->val == val)
 			return curr;
 
 		if (curr->bottom)
@@ -120,83 +143,169 @@ struct item* search_key(struct skip_list* list, int val){
 	return NULL;
 }
 
-//TODO: Remove
-struct item* search_to_insert_in_tower(struct item *node, int val) {
-
-	struct item *curr = node; // Start at top-left node
-
-	for (;;) {
-		if (curr->next && val >= curr->next->val)
-			curr = curr->next;
-		else
-			return curr;
-	}
-
-	// DEADC0DE
-	return NULL;
-}
-
-void print_list(struct skip_list* curr_list){
+void print_list(struct skip_list *curr_list) {
 	struct tower *curr_tower = curr_list->top_tower;
-		struct item *curr_item;
+	struct item *curr_item;
 
-		while ( 1 ) {
-			curr_item = curr_tower->items->next;
-			printf("Tower number %d \n ", curr_tower->height);
-			while (curr_item) {
-				printf(" %d ", curr_item->val);
-				curr_item = curr_item->next;
-			}
-			printf("\n");
-
-			if(curr_tower->items->bottom)
-				curr_tower = curr_tower->items->bottom->assigned;
-			else
-				break;
+	while (1) {
+		curr_item = curr_tower->items->next;
+		printf("Tower number %d \n ", curr_tower->height);
+		while (curr_item) {
+			printf(" %d ", curr_item->val);
+			curr_item = curr_item->next;
 		}
+		printf("\n");
+
+		if (curr_tower->items->bottom)
+			curr_tower = curr_tower->items->bottom->assigned;
+		else
+			break;
+	}
 
 }
 
 struct item* insert_node(struct skip_list *list, int val) {
 	printf("Inserting element: %d\n", val);
-	struct item *found = search_to_insert(list->top_tower->items, val); // it should be at level 0
-	struct item *added = append_node(found, val, NULL);
+	struct insert_history *hist = NULL, *prev_hist;
+	struct item *found = search_to_insert(list->top_tower->items, val, &hist);
+	// Found should be at level 0!
 
-	struct item *added_prev = added;
+	struct item *added = append_node(found, val);
+
+	struct item *curr_item = added;
 	struct tower *curr_tower = added->assigned;
 
+	struct item *prev_item = added;
 	while (head_or_tail()) {
 		if (!curr_tower->prev) {
+			// We are going to create a new tower, there is only one start node
 			list->top_tower = create_tower(curr_tower->height + 1, curr_tower);
 			curr_tower->prev = list->top_tower;
-		}
-		// TODO: Use history from searching from insert, to append proper nodes
-		struct item *added_higher = append_node(
-				search_to_insert_in_tower(curr_tower->prev->items, val), val,
-				added_prev);
 
-		added_prev = added_higher;
-		curr_tower = added_higher->assigned;
+			// Append new one to it
+			curr_item = append_node(list->top_tower->items, val);
+			list->top_tower->items->next->bottom = prev_item;
+
+		} else {
+			// We are at existing higher tower
+			// Recover the last node from which we went down
+			// Append a new node to it
+			curr_item = append_node(hist->inserted, val);
+			curr_item->bottom = prev_item;
+
+			prev_hist = hist;
+			hist = hist->next;
+			free(prev_hist);
+		}
+
+		prev_item = curr_item;
+		curr_tower = curr_item->assigned;
 	}
+
+	// Remove leftovers if we are not going to
+	// add another node to the higher level
+
+	while (hist) {
+		prev_hist = hist;
+		hist = hist->next;
+		free(prev_hist);
+	}
+
 	printf("\n");
 	print_list(list);
 	printf("\n");
-	return added_prev;
+	return found;
+}
+
+void delete_tower(struct tower *to_del) {
+	struct item *curr_item = to_del->items;
+	struct item *next_item = curr_item;
+
+	do {
+
+		curr_item = next_item;
+		next_item = curr_item->next;
+
+		curr_item->next = NULL;
+		curr_item->bottom = NULL;
+		curr_item->assigned = NULL;
+
+		free(curr_item);
+	} while (next_item);
+
+	to_del->prev = NULL;
+	to_del->items = NULL;
+	free(to_del);
+}
+
+void delete_node(struct item *to_del_after) {
+	struct item *temp = to_del_after->next->next;
+
+	to_del_after->next->next = NULL;
+	to_del_after->next->bottom = NULL;
+
+	free(to_del_after->next);
+
+	if (temp)
+		to_del_after->next = temp;
+	else
+		to_del_after->next = NULL;
 }
 
 
+//TODO: Make deletion of a node, not value (allow duplicates).
+void delete_node_from_list_p(struct items* item,  struct item* to_del) {}
 
+
+void delete_node_from_list(struct skip_list *skip, int val) {
+	struct item *curr = skip->top_tower->items;
+	struct item *curr_bottom = NULL;
+
+	for (;;) {
+
+		while (curr->next && curr->next->val < val)
+			curr = curr->next;
+
+		if (curr->next && curr->next->val == val) {
+			delete_node(curr);
+			// Removing empty tower
+			if (curr == curr->assigned->items && !curr->next) {
+				curr_bottom = curr->bottom;
+				delete_tower(curr->assigned);
+
+				skip->top_tower = curr_bottom->assigned;
+				curr_bottom->assigned->prev = NULL;
+
+				if (curr_bottom) {
+					curr = curr_bottom;
+					continue;
+				} else {
+					return;
+				}
+			}
+
+		}
+
+		if (curr->bottom)
+			curr = curr->bottom;
+		else
+			return;
+	}
+
+}
 int main(void) {
-	srand( 0 );		// RANDOMIZATION
+	srand(0);		// RANDOMIZATION
 	struct skip_list *skip = create_skip_list();
-	for(int i = 0 ; i < 1000; i++)
-	insert_node(skip, rand() % 4096);
 
-	// Print list
+	for (int i = 0; i < 60; i++)
+		insert_node(skip, rand() % 1024);
 
-	struct item* found_k = search_key(skip, 103);
+	delete_node_from_list(skip, 153);
+	print_list(skip);
 
-	if(found_k)
+	struct item *found_k = search_key(skip, 923);
+
+	if (found_k)
 		printf("Found! %d Level %d", found_k->val, found_k->assigned->height);
 
 	return 0;
